a13497b75c3977ab134cab7ff2ab01e6756c040d
frcihan <ferruhcihan20@gmail.com>
2022-01-18 22:55:51 +0300

    feat:update header component

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,17 @@
-import Image from "next/image"
+import Image from "next/image";
 
 function Header() {
-    return (
-        <header className="">
-            <Image className="object-contain" src="https://links.papareact.com/ua6" width={200} height={100} />
-        </header>
-    )
+  return (
+    <header className="">
+      <Image
+        className="object-contain"
+        src="https://links.papareact.com/ua6"
+        width={200}
+        height={100}
+        alt="header-logo"
+      />
+    </header>
+  );
 }
 
-export default Header
+export default Header;
