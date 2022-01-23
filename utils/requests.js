f2f355737775ef306dbb7bf62e68984ce093b397
frcihan <ferruhcihan20@gmail.com>
2022-01-23 23:41:22 +0300

    feat:update requests

--- utils/requests.js
+++ utils/requests.js
@@ -1,1 +1,21 @@
+/* eslint-disable import/no-anonymous-default-export */
 const API_KEY = process.env.API_KEY;
+
+export default {
+  fetchTrending: {
+    title: "Trending",
+    url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchTopRated: {
+    title: "Top Rated",
+    url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchActionMovies: {
+    title: "Action",
+    url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchComedyMovies: {
+    title: "Comedy",
+    url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  },
+};
