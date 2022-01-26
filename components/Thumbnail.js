8ebff851527169b6d256cc5eac04fe732e10d0cb
frcihan <ferruhcihan20@gmail.com>
2022-01-26 23:25:08 +0300

    feat:add thumbnail image

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,6 +1,14 @@
+import Image from "next/image";
+
 const Thumbnail = ({result}) => {
+  const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return <div>
-      <h1>{result.title}</h1>
+      <Image 
+        layout="responsive"
+        src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`}
+        height={1080}
+        width={1920}
+      />
   </div>;
 };
 
