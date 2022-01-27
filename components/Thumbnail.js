38e69fd7b2572f342155139886bd550a4a290e3a
frcihan <ferruhcihan20@gmail.com>
2022-01-28 01:32:08 +0300

    feat:update thumbnail comp

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -3,7 +3,7 @@ import {ThumbUpIcon} from "@heroicons/react/outline";
 
 const Thumbnail = ({result}) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
-  return <div className="group cursor-pointer">
+  return <div className="p-2 group cursor-pointer transition duration-200 ease-in sm:hover:scale-105 hover:z-50">
       <Image 
         layout="responsive"
         src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`}
@@ -12,7 +12,7 @@ const Thumbnail = ({result}) => {
       />
       <div className="p-2">
         <p className="truncate max-w-md">{result.overview}</p>
-        <h2>{result.title || result.original_name}</h2>
+        <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">{result.title || result.original_name}</h2>
         <p className="flex items-center opacity-0 group-hover:opacity-100">
           {result.media_type && `${result.media_type} •`}{" "}
           {result.release_date || result.first_air_date} •{" "}
